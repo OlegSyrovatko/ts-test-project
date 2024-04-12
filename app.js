45f6f3d0a70ff5a5d8b2055c7c884ccb85102edf
Oleg <sirov@ukr.net>
2024-04-12 22:47:51 +0300

    add test number

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("test !");
+console.log("test!");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 5;
+var n2 = 5.5;
+var result = add(n1, n2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log("test !");
+console.log("test!");
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+ 
+const n1 = 5;
+const n2 = 5.5;
+
+const result = add(n1, n2);
+console.log(result);
