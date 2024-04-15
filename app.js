b4e489951f3296e927d0c70ce381a15d30d77d5e
Oleg <sirov@ukr.net>
2024-04-15 12:49:38 +0300

    add enum type

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-var person = {
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "admin";
+    Role[Role["READ_ONLY"] = 5] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 200] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person 
+/*
+ : {
+ name: string;
+ age: number;
+ hobbies: string[]; // array
+ // role: [number, string]; // tuple (fixed length)
+ }
+ */
+= {
     name: "Oleh",
     age: 40,
     hobbies: ['sports', 'books'],
-    role: [2, 'author']
+    // role: [2, 'author']
+    role: Role.ADMIN
 };
 console.log(person.name);
 console.log(person.role);
--- app.ts
+++ app.ts
@@ -1,14 +1,23 @@
-const person: {
+enum Role { ADMIN = "admin", READ_ONLY = 5, AUTHOR = 200 };
+
+const person
+   /* 
+    : {
     name: string;
     age: number;
     hobbies: string[]; // array
-    role: [number, string]; // tuple (fixed length)
-} = {
+    // role: [number, string]; // tuple (fixed length)
+    }
+    */
+    =
+{
     name: "Oleh",
     age: 40,
     hobbies: ['sports', 'books'],
-    role: [2, 'author']
+    // role: [2, 'author']
+    role: Role.ADMIN
 }
+
 
 console.log(person.name);
 console.log(person.role);
