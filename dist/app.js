08669b4cadd951b43ee8673350458742ff994fd6
Oleg <sirov@ukr.net>
2024-04-22 13:23:12 +0300

    add private modifiers

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,22 @@
 "use strict";
 class Department {
     constructor(n) {
+        this.emploees = [];
         this.name = n;
     }
     describes() {
         console.log("describes: " + this.name);
     }
+    addEmployee(emploee) {
+        this.emploees.push(emploee);
+    }
+    showEmployees() {
+        console.log(this.emploees.length);
+        console.log(this.emploees);
+    }
 }
 const accaunting = new Department('Accaunting');
-accaunting.describes();
-const accauntingCopy = { name: "AccauntingCopy", describes: accaunting.describes };
-accauntingCopy.describes();
+accaunting.addEmployee("Oleh");
+accaunting.addEmployee("Max");
+accaunting.showEmployees();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,30 @@
 class Department {
     name: string;
+    private emploees: string[] = [];
     constructor(n: string) {
         this.name = n;
     }
     
     describes(this: Department) {
         console.log("describes: " + this.name);
-        
-     }
+    }
+    addEmployee(emploee: string) {
+        this.emploees.push(emploee);
+    }
+    showEmployees (){
+        console.log(this.emploees.length);
+        console.log(this.emploees);
+   }
 }
  
 const accaunting = new Department('Accaunting');
 
-accaunting.describes();
+// accaunting.describes();
 
-const accauntingCopy = {name: "AccauntingCopy", describes: accaunting.describes };
-accauntingCopy.describes();
+// const accauntingCopy = {name: "AccauntingCopy", describes: accaunting.describes };
+// accauntingCopy.describes();
+
+accaunting.addEmployee("Oleh");
+accaunting.addEmployee("Max");
+// accaunting.emploees[2] = "Anna";
+accaunting.showEmployees();
