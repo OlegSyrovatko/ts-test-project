2ac0ca84c748f5151622c2fecfd951f2b1e7702a
Oleg <sirov@ukr.net>
2024-04-22 17:14:29 +0300

    add Inheritance

--- dist/app.js
+++ dist/app.js
@@ -16,9 +16,33 @@ class Department {
         console.log(this.emploees);
     }
 }
-const accaunting = new Department('d1', 'Accaunting');
-accaunting.describes();
-accaunting.addEmployee("Oleh");
-accaunting.addEmployee("Max");
-accaunting.showEmployees();
+class ItDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT");
+        this.admins = [];
+        this.admins = admins;
+    }
+}
+class AccountDepartment extends Department {
+    constructor(id, reports) {
+        super(id, "Account");
+        this.reports = reports;
+        this.reports = reports;
+    }
+    addReport(report) {
+        this.reports.push(report);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const ItDep = new ItDepartment("d1", ["John"]);
+ItDep.describes();
+ItDep.addEmployee("John");
+ItDep.addEmployee("Max");
+ItDep.showEmployees();
+console.log(ItDep);
+const AcDep = new AccountDepartment("d2", []);
+AcDep.addReport("first report");
+AcDep.printReports();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,63 @@
 class Department {
-    // private readonly id: string;
-    // private name: string;
+  // private readonly id: string;
+  // private name: string;
 
-    private emploees: string[] = [];
+  private emploees: string[] = [];
 
-    constructor(private readonly id: string, private name: string) {
-        // this.id = id;
-        // this.name = name;
-    }
-    
-    describes(this: Department) {
-        console.log("describes: " + this.id + " " + this.name);
-    }
-    addEmployee(emploee: string) {
-        this.emploees.push(emploee);
-    }
-    showEmployees (){
-        console.log(this.emploees.length);
-        console.log(this.emploees);
-   }
+  constructor(private readonly id: string, private name: string) {
+    // this.id = id;
+    // this.name = name;
+  }
+
+  describes(this: Department) {
+    console.log("describes: " + this.id + " " + this.name);
+  }
+  addEmployee(emploee: string) {
+    this.emploees.push(emploee);
+  }
+  showEmployees() {
+    console.log(this.emploees.length);
+    console.log(this.emploees);
+  }
 }
- 
-const accaunting = new Department('d1', 'Accaunting');
 
-accaunting.describes();
+class ItDepartment extends Department {
+  admins: string[] = [];
+
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Account");
+    this.reports = reports;
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const ItDep = new ItDepartment("d1", ["John"]);
+
+ItDep.describes();
 
 // const accauntingCopy = {name: "AccauntingCopy", describes: accaunting.describes };
 // accauntingCopy.describes();
 
-accaunting.addEmployee("Oleh");
-accaunting.addEmployee("Max");
+ItDep.addEmployee("John");
+ItDep.addEmployee("Max");
 // accaunting.emploees[2] = "Anna";
-accaunting.showEmployees();
+ItDep.showEmployees();
+console.log(ItDep);
+
+const AcDep = new AccountDepartment("d2", []);
+
+AcDep.addReport("first report");
+AcDep.printReports();
