9093aa3084c443cb43f0184edc3d0053238b5114
Oleg <sirov@ukr.net>
2024-04-29 17:20:36 +0300

    add keyof

--- dist/app.js
+++ dist/app.js
@@ -4,4 +4,23 @@ function merge(objA, objB) {
 }
 const mergedObj = merge({ name: "Oleh", hobbies: ["sports"] }, { age: 30 });
 console.log(mergedObj);
+function countAndDescribe(element) {
+    let descriptText = "empty";
+    if (element.length === 1) {
+        descriptText = "Got 1 element";
+    }
+    if (element.length > 1) {
+        descriptText = "Got " + element.length + " elements";
+    }
+    return [element, descriptText];
+}
+console.log(countAndDescribe(""));
+console.log(countAndDescribe("h"));
+console.log(countAndDescribe("high there"));
+console.log(countAndDescribe([]));
+console.log(countAndDescribe(["sport", "cooking"]));
+function keyInObject(obj, key) {
+    return "value " + obj[key];
+}
+console.log(keyInObject({ name: "Oleh" }, "name"));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -17,3 +17,28 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 const mergedObj = merge({ name: "Oleh", hobbies: ["sports"] }, { age: 30 });
 
 console.log(mergedObj);
+
+interface Lengthy {
+  length: number;
+}
+function countAndDescribe<T extends Lengthy>(element: T) {
+  let descriptText = "empty";
+  if (element.length === 1) {
+    descriptText = "Got 1 element";
+  }
+  if (element.length > 1) {
+    descriptText = "Got " + element.length + " elements";
+  }
+  return [element, descriptText];
+}
+console.log(countAndDescribe(""));
+console.log(countAndDescribe("h"));
+console.log(countAndDescribe("high there"));
+console.log(countAndDescribe([]));
+console.log(countAndDescribe(["sport", "cooking"]));
+
+function keyInObject<T extends object, U extends keyof T>(obj: T, key: U) {
+  return "value " + obj[key];
+}
+
+console.log(keyInObject({ name: "Oleh" }, "name"));
