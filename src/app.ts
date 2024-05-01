892d3272f209913a7ab996b8d58f85dce43c41b3
Oleg <sirov@ukr.net>
2024-05-01 23:32:02 +0300

    Building More Useful Decorators

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,29 @@
-// function Logger(constructor: Function) {
-//   console.log("logging");
-//   console.log(constructor);
-// }
-
-// @Logger
+function Logger0(constructor: Function) {
+  console.log("logging");
+  console.log(constructor);
+}
 
 function Logger(param: string) {
-  return function (constructor: Function) {
+  return function (_: Function) {
     console.log(param);
-    console.log(constructor);
   };
 }
 
+function WithTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+@WithTemplate("<h1>default name</h1>", "app")
+@Logger0
 @Logger("logging-argument")
 class Person2 {
-  name = "Max";
+  name = "Oleh";
 
   constructor() {
     console.log("Creating Object");
