e4c215b31446e0007b9bb46361e6aac4ceaea188
Oleg <sirov@ukr.net>
2024-05-03 23:10:57 +0300

    Property Decorators

--- dist/app.js
+++ dist/app.js
@@ -6,15 +6,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 function Logger0(constructor) {
-    console.log("logging");
+    console.log("logging0");
     console.log(constructor);
 }
 function Logger(param) {
+    console.log("logger");
     return function (_) {
         console.log(param);
     };
 }
 function WithTemplate(template, hookId) {
+    console.log("add h1");
     return function (constructor) {
         const hookEl = document.getElementById(hookId);
         const p = new constructor();
@@ -37,4 +39,28 @@ Person2 = __decorate([
 ], Person2);
 const pers = new Person2();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log("Property decorator");
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error("Price should be great than zero");
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 function Logger0(constructor: Function) {
-  console.log("logging");
+  console.log("logging0");
   console.log(constructor);
 }
 
 function Logger(param: string) {
+  console.log("logger");
   return function (_: Function) {
     console.log(param);
   };
 }
 
 function WithTemplate(template: string, hookId: string) {
+  console.log("add h1");
   return function (constructor: any) {
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
@@ -32,3 +34,30 @@ class Person2 {
 
 const pers = new Person2();
 console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator");
+  console.log(target, propertyName);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Price should be great than zero");
+    }
+  }
+
+  getPriceWithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
