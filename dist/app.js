fc56e39624ee4425fb18eb0b0a2599de595cebcb
Oleg <sirov@ukr.net>
2024-05-10 20:21:29 +0300

    Rendering Project Lists

--- dist/app.js
+++ dist/app.js
@@ -40,6 +40,27 @@ function autobind(_, _2, descriptor) {
     };
     return adjDescriptor;
 }
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById("project-list");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent =
+            this.type.toUpperCase() + " PROJECTS";
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById("project-input");
@@ -100,11 +121,13 @@ class ProjectInput {
         this.element.addEventListener("submit", this.submitHandler);
     }
     attach() {
-        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
     }
 }
 __decorate([
     autobind
 ], ProjectInput.prototype, "submitHandler", null);
 const myForm = new ProjectInput();
+const activeProjlist = new ProjectList("active");
+const finishedProjlist = new ProjectList("finished");
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -50,6 +50,39 @@ function autobind(_: any, _2: string | Symbol, descriptor: PropertyDescriptor) {
   return adjDescriptor;
 }
 
+class ProjectList {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor(private type: "active" | "finished") {
+    this.templateElement = document.getElementById(
+      "project-list"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent =
+      this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
+  }
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -143,8 +176,10 @@ class ProjectInput {
   }
 
   private attach() {
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
   }
 }
 
 const myForm = new ProjectInput();
+const activeProjlist = new ProjectList("active");
+const finishedProjlist = new ProjectList("finished");
