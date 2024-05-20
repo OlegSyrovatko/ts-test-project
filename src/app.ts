3169ff1e8caea9e9c52c046ec386a58959975f03
Oleg <sirov@ukr.net>
2024-05-20 13:03:04 +0300

    finish

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+// /// <reference path = "./components/project-list.ts" />
+// /// <reference path = "./components/project-input.ts" />
+
+// import { ProjectList } from "./components/project-list.js";
+// import { ProjectInput } from "./components/project-input.js";
+
+import { ProjectList } from "./components/project-list";
+import { ProjectInput } from "./components/project-input";
+
+// namespace App for my project {
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
+// }
